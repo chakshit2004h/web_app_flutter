e3192ac3065ebd455c11d509f14a665aa863dc69
chakshit2004h <84057123+chakshit2004h@users.noreply.github.com>
2025-04-22 11:58:28 +0530

    first commit

--- lib/addon/side_bar_items.dart
+++ lib/addon/side_bar_items.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:web_app/required_pages/gps_enable_disable.dart';
 
-class SideBar extends StatelessWidget {
+class SideBar extends StatefulWidget {
   final String selectedItem;
   final Function(String) updateSelection;
   final VoidCallback onArrowTap;
@@ -14,85 +13,172 @@ class SideBar extends StatelessWidget {
   });
 
   @override
+  State<SideBar> createState() => _SideBarState();
+}
+
+class _SideBarState extends State<SideBar> {
+  bool isSearchActive = false;
+  TextEditingController _searchController = TextEditingController();
+  String searchQuery = '';
+
+  final List<String> sidebarItems = [
+    'Search',
+    'Traceroute',
+    'GPS Enable/Disable',
+    'Voice + FTP Upload',
+    'Tcpdump Record',
+    'Require Cell File',
+    'LTE EARFCN Lock',
+    'OTT Video Streaming MOS',
+    'Ask for site completion after script ended',
+    'Set Variable Statement',
+    'Modem Data Packet Logging Enable',
+    'LINE Receive',
+    'Dynamic GSM Cell Info',
+    'Facebook (Post Video)',
+    'Pause',
+    'Instagram post video',
+    'Dynamic WCDMA Cell Info',
+    'Instagram post comment',
+    'BEC Router Diagnostic Config',
+    'Browser',
+    'Data Enable/Disable',
+    'Send Email',
+    'Ping',
+    'OTT Video Streaming',
+    'Play Facebook Video',
+    'nPerf Test',
+    'DoH Lookup',
+    'YouTube (Upload Video)',
+    'Send SMS',
+    'WhatsApp send message',
+    'WiFi Scan Enable/Disable',
+    'Send MMS',
+    'Facebook (Post Status Update)',
+    'WiFi Connect',
+    'SFTP Upload',
+    'FTP Upload',
+    'Facebook (Post Photo)',
+    'LINE Send Identifier Test',
+    'Set Log Server',
+    'Answer',
+    'Dropbox Download',
+    'Instagram post photo',
+    'Set APN',
+    'Conditional Loop',
+    'Dropbox Upload',
+    'R99 Lock Enable/Disable',
+    'Temporary Airplane Mode',
+    'Wait for PCI',
+  ];
+
+  @override
   Widget build(BuildContext context) {
+    List<String> filteredItems = sidebarItems
+        .where((item) => item.toLowerCase().contains(searchQuery.toLowerCase()))
+        .toList();
+
     return Row(
       children: [
-        // Sidebar list
         Container(
           width: 250,
-          color: Color(0xff1a1e22),
-          child: ListView(
-            padding: EdgeInsets.all(8),
+          color: const Color(0xff1a1e22),
+          child: Column(
             children: [
-              SidebarItem(title: 'Search', isSelected: selectedItem == 'Search', onTap: () => updateSelection('Search')),
-              SidebarItem(title: 'Traceroute', isSelected: selectedItem == 'Traceroute', onTap: () => updateSelection('Traceroute')),
-              SidebarItem(title: 'GPS Enable/Disable', isSelected: selectedItem == 'GPS Enable/Disable', onTap: () => updateSelection('GPS Enable/Disable')),
-              SidebarItem(title: 'Voice + FTP Upload', isSelected: selectedItem == 'Voice + FTP Upload', onTap: () => updateSelection('Voice + FTP Upload')),
-              SidebarItem(title: 'Tcpdump Record', isSelected: selectedItem == 'Tcpdump Record', onTap: () => updateSelection('Tcpdump Record')),
-              SidebarItem(title: 'Require Cell File', isSelected: selectedItem == 'Require Cell File', onTap: () => updateSelection('Require Cell File')),
-              SidebarItem(title: 'LTE EARFCN Lock', isSelected: selectedItem == 'LTE EARFCN Lock', onTap: () => updateSelection('LTE EARFCN Lock')),
-              SidebarItem(title: 'OTT Video Streaming MOS', isSelected: selectedItem == 'OTT Video Streaming MOS', onTap: () => updateSelection('OTT Video Streaming MOS')),
-              SidebarItem(title: 'Ask for site completion after script ended', isSelected: selectedItem == 'Ask for site completion after script ended', onTap: () => updateSelection('Ask for site completion after script ended')),
-              SidebarItem(title: 'Set Variable Statement', isSelected: selectedItem == 'Set Variable Statement', onTap: () => updateSelection('Set Variable Statement')),
-              SidebarItem(title: 'Modem Data Packet Logging Enable', isSelected: selectedItem == 'Modem Data Packet Logging Enable', onTap: () => updateSelection('Modem Data Packet Logging Enable')),
-              SidebarItem(title: 'LINE Receive', isSelected: selectedItem == 'LINE Receive', onTap: () => updateSelection('LINE Receive')),
-              SidebarItem(title: 'Dynamic GSM Cell Info', isSelected: selectedItem == 'Dynamic GSM Cell Info', onTap: () => updateSelection('Dynamic GSM Cell Info')),
-              SidebarItem(title: 'Facebook (Post Video)', isSelected: selectedItem == 'Facebook (Post Video)', onTap: () => updateSelection('Facebook (Post Video)')),
-              SidebarItem(title: 'Pause', isSelected: selectedItem == 'Pause', onTap: () => updateSelection('Pause')),
-              SidebarItem(title: 'Instagram post video', isSelected: selectedItem == 'Instagram post video', onTap: () => updateSelection('Instagram post video')),
-              SidebarItem(title: 'Dynamic WCDMA Cell Info', isSelected: selectedItem == 'Dynamic WCDMA Cell Info', onTap: () => updateSelection('Dynamic WCDMA Cell Info')),
-              SidebarItem(title: 'Instagram post comment', isSelected: selectedItem == 'Instagram post comment', onTap: () => updateSelection('Instagram post comment')),
-              SidebarItem(title: 'BEC Router Diagnostic Config', isSelected: selectedItem == 'BEC Router Diagnostic Config', onTap: () => updateSelection('BEC Router Diagnostic Config')),
-              SidebarItem(title: 'Browser', isSelected: selectedItem == 'Browser', onTap: () => updateSelection('Browser')),
-              SidebarItem(title: 'Data Enable/Disable', isSelected: selectedItem == 'Data Enable/Disable', onTap: () => updateSelection('Data Enable/Disable')),
-              SidebarItem(title: 'Send Email', isSelected: selectedItem == 'Send Email', onTap: () => updateSelection('Send Email')),
-              SidebarItem(title: 'Ping', isSelected: selectedItem == 'Ping', onTap: () => updateSelection('Ping')),
-              SidebarItem(title: 'OTT Video Streaming', isSelected: selectedItem == 'OTT Video Streaming', onTap: () => updateSelection('OTT Video Streaming')),
-              SidebarItem(title: 'Play Facebook Video', isSelected: selectedItem == 'Play Facebook Video', onTap: () => updateSelection('Play Facebook Video')),
-              SidebarItem(title: 'nPerf Test', isSelected: selectedItem == 'nPerf Test', onTap: () => updateSelection('nPerf Test')),
-              SidebarItem(title: 'DoH Lookup', isSelected: selectedItem == 'DoH Lookup', onTap: () => updateSelection('DoH Lookup')),
-              SidebarItem(title: 'YouTube (Upload Video)', isSelected: selectedItem == 'YouTube (Upload Video)', onTap: () => updateSelection('YouTube (Upload Video)')),
-              SidebarItem(title: 'Send SMS', isSelected: selectedItem == 'Send SMS', onTap: () => updateSelection('Send SMS')),
-              SidebarItem(title: 'WhatsApp send message', isSelected: selectedItem == 'WhatsApp send message', onTap: () => updateSelection('WhatsApp send message')),
-              SidebarItem(title: 'WiFi Scan Enable/Disable', isSelected: selectedItem == 'WiFi Scan Enable/Disable', onTap: () => updateSelection('WiFi Scan Enable/Disable')),
-              SidebarItem(title: 'Send MMS', isSelected: selectedItem == 'Send MMS', onTap: () => updateSelection('Send MMS')),
-              SidebarItem(title: 'Facebook (Post Status Update)', isSelected: selectedItem == 'Facebook (Post Status Update)', onTap: () => updateSelection('Facebook (Post Status Update)')),
-              SidebarItem(title: 'WiFi Connect', isSelected: selectedItem == 'WiFi Connect', onTap: () => updateSelection('WiFi Connect')),
-              SidebarItem(title: 'SFTP Upload', isSelected: selectedItem == 'SFTP Upload', onTap: () => updateSelection('SFTP Upload')),
-              SidebarItem(title: 'FTP Upload', isSelected: selectedItem == 'FTP Upload', onTap: () => updateSelection('FTP Upload')),
-              SidebarItem(title: 'Facebook (Post Photo)', isSelected: selectedItem == 'Facebook (Post Photo)', onTap: () => updateSelection('Facebook (Post Photo)')),
-              SidebarItem(title: 'LINE Send Identifier Test', isSelected: selectedItem == 'LINE Send Identifier Test', onTap: () => updateSelection('LINE Send Identifier Test')),
-              SidebarItem(title: 'Set Log Server', isSelected: selectedItem == 'Set Log Server', onTap: () => updateSelection('Set Log Server')),
-              SidebarItem(title: 'Answer', isSelected: selectedItem == 'Answer', onTap: () => updateSelection('Answer')),
-              SidebarItem(title: 'Dropbox Download', isSelected: selectedItem == 'Dropbox Download', onTap: () => updateSelection('Dropbox Download')),
-              SidebarItem(title: 'Instagram post photo', isSelected: selectedItem == 'Instagram post photo', onTap: () => updateSelection('Instagram post photo')),
-              SidebarItem(title: 'Set APN', isSelected: selectedItem == 'Set APN', onTap: () => updateSelection('Set APN')),
-              SidebarItem(title: 'Conditional Loop', isSelected: selectedItem == 'Conditional Loop', onTap: () => updateSelection('Conditional Loop')),
-              SidebarItem(title: 'Dropbox Upload', isSelected: selectedItem == 'Dropbox Upload', onTap: () => updateSelection('Dropbox Upload')),
-              SidebarItem(title: 'R99 Lock Enable/Disable', isSelected: selectedItem == 'R99 Lock Enable/Disable', onTap: () => updateSelection('R99 Lock Enable/Disable')),
-              SidebarItem(title: 'Temporary Airplane Mode', isSelected: selectedItem == 'Temporary Airplane Mode', onTap: () => updateSelection('Temporary Airplane Mode')),
-              SidebarItem(title: 'Wait for PCI', isSelected: selectedItem == 'Wait for PCI', onTap: () => updateSelection('Wait for PCI')),
+              if (isSearchActive)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: 'Search...',
+                      hintStyle: TextStyle(color: Colors.white70),
+                      filled: true,
+                      fillColor: Color(0xff03120e),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      prefixIcon: Icon(Icons.search, color: Colors.white),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.clear, color: Colors.white),
+                        onPressed: () {
+                          _searchController.clear();
+                          setState(() {
+                            searchQuery = '';
+                          });
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.teal),
+                      ),
+                    ),
+                    style: TextStyle(color: Colors.white),
+                    onChanged: (val) {
+                      setState(() {
+                        searchQuery = val;
+                      });
+                    },
+                  )
+
+                ),
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: filteredItems.length,
+                  itemBuilder: (context, index) {
+                    String item = filteredItems[index];
+                    return SidebarItem(
+                      title: item,
+                      isSelected: widget.selectedItem == item,
+                      onTap: () {
+                        if (item == 'Search') {
+                          setState(() {
+                            isSearchActive = !isSearchActive;
+                            if (!isSearchActive) {
+                              _searchController.clear();
+                              searchQuery = '';
+                            }
+                          });
+                        } else {
+                          setState(() {
+                            isSearchActive = false;
+                            searchQuery = '';
+                            _searchController.clear();
+                          });
+                        }
+                        widget.updateSelection(item);
+                      },
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
-        // Circular background for arrow
+        // Arrow button
         Container(
           width: 50,
-          color: Color(0xff1a1e22),
+          color: const Color(0xff1a1e22),
           child: Center(
             child: GestureDetector(
-              onTap: onArrowTap,
+              onTap: widget.onArrowTap,
               child: Container(
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   color: Colors.black,
                   shape: BoxShape.circle,
                 ),
-                padding: EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
                 child: IconButton(
-                  onPressed: onArrowTap,
-                  icon: Icon(Icons.arrow_forward_ios,
-                    color: Colors.teal,
-                    size: 16,)
+                  onPressed: widget.onArrowTap,
+                  icon: const Icon(Icons.arrow_forward_ios,
+                      color: Colors.teal, size: 16),
                 ),
               ),
             ),
@@ -102,12 +188,13 @@ class SideBar extends StatelessWidget {
     );
   }
 }
+
 class SidebarItem extends StatelessWidget {
   final String title;
   final bool isSelected;
   final VoidCallback onTap;
 
-  SidebarItem({
+  const SidebarItem({
     required this.title,
     required this.isSelected,
     required this.onTap,
@@ -118,7 +205,7 @@ class SidebarItem extends StatelessWidget {
     return Card(
       color: isSelected ? Colors.teal : Colors.black54,
       child: ListTile(
-        title: Text(title, style: TextStyle(color: Colors.white)),
+        title: Text(title, style: const TextStyle(color: Colors.white)),
         onTap: onTap,
       ),
     );
